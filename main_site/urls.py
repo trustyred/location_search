a571075e8edd88ca5706925c3a72519ffeae2897
trustyred <244073249@qq.com>
2018-03-14 20:07:47 +0800

    location_search project git init

---
+++ main_site/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url, include
+from main_site.views import main, getBikeLatLng, signUp, login, test_sign, isLogin, getPhoneId
+
+urlpatterns = [
+    url(r'test/$', main),
+    url(r'getBikeLatLng', getBikeLatLng),
+    url(r'signUp/$', signUp),
+    url(r'login/$', login),
+    url(r'test_sign/$', test_sign),
+    url(r'isLogin/$', isLogin),
+    url(r'getPhoneId/$', getPhoneId),
+
+]
